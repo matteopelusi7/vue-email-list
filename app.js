4a8826d5fa98e66f8b34f0a7e66be9abd775b65c
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-04 16:36:31 +0100

    Aggiunte mail

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const app = new Vue({
     el: '#app',
     data: {
 
-
-
+        numEmail: 10,
+        arrayEmail: [],
+        
     },
-
+    
     created() {
+        
+        for( let i = 0; i < this.numEmail; i++ ){
 
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (res) => {
+    
+                this.arrayEmail.push(res.data.response)
 
-
+            })
+            
+        }
+        
     }
 
 });
